dfe22338661fa2b97048d511d5b3648fe0867da6
Artem <97912920+underwhot@users.noreply.github.com>
2022-09-26 03:08:57 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    window.addEventListener("scroll", (function() {
+        document.documentElement.style.setProperty("--scrollTop", `${this.scrollY}px`);
+    }));
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+    ScrollSmoother.create({
+        wrapper: ".wrapper",
+        content: ".content"
+    });
+    window["FLS"] = true;
+    isWebp();
+})();
